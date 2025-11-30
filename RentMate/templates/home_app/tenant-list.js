6be507a20b02a1f0380177ae2701ef11ff457148
IzPaul <junpaularradaza9@gmail.com>
2025-11-30 21:51:39 +0800

    removed unneccsary stuff

--- RentMate/templates/home_app/tenant-list.js
+++
@@ -1,6 +0,0 @@
-searchInput.addEventListener('keydown', function(e) {
-    if (e.key === 'Escape') {
-        searchInput.value = '';
-        searchInput.dispatchEvent(new Event('input')); // trigger search clear
-    }
-});
